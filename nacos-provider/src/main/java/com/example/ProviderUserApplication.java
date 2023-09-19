98f4b6e237c16b1c1c2403035382dbfac0a20094
Haoran Liu <2785456L@student.gla.ac.uk>
2023-09-19 13:57:42 +0100

    initial commit

--- nacos-provider/src/main/java/com/example/ProviderUserApplication.java
+++ nacos-provider/src/main/java/com/example/ProviderUserApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //@EnableJpaRepositories("com.example.repository")
 public class ProviderUserApplication {
 	public static void main(String[] args){
+		System.out.println("Hi");
 		SpringApplication.run(ProviderUserApplication.class, args);
 	}
 
